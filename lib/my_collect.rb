e2b40d637899b9f7544ec5eb1ed3cbfbaef4a846
zhenggao cai <caizhengao1992@gmail.com>
2019-04-19 03:04:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
-
-
+def collect(array_name)
+  i = 0 
+  collection=[]
+  while i < array_name.size 
+  yield array_name[i]
+  collection << array_name[i]
+  i += 1 
+end 
+return collection
+end
+def my_collect(array)
+  new_array=[]
+  i = 0
+  while i< array.size 
+  yield array[i]
+  new_array[i]=array[i].split(" ").first
+  i += 1 
+  end
+  return array[i]
+end 
